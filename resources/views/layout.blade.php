ea95226f884102e58bbc60e8b2c6a1d4d970299f
daniellie31 <danielll3112@gmail.com>
2021-12-04 00:53:10 +0700

    add footer and signIn blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,23 +9,34 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">GameSLot</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">GameSLot</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto ml-auto">
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                </form>
-            </ul>
-            <button type="button" class="btn btn-primary mr-2">Sign in</button>
-            <button type="button" class="btn btn-outline-primary">Sign up</button>
-            {{-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> --}}
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto ml-auto">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </ul>
+                <button type="submit" class="btn btn-primary mr-2">Sign in</button>
+                <button type="submit" class="btn btn-outline-primary">Sign up</button>
+                {{-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> --}}
+            </div>
+        </nav>
+    </header>
 
+    <main>
+        @yield('content')
+    </main>
+
+    <footer class="footer">
+        <div class="container text-center py-3">
+            <span class="text-muted">&copy 2021 SL, Inc. All rights reserved.</span>
         </div>
-    </nav>
+    </footer>
 </body>
 </html>
