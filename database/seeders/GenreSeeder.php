fdaf0721eb5cdf7725ac93c3b3a9bc26694202c6
daniellie31 <danielll3112@gmail.com>
2021-12-07 21:28:29 +0700

    addgame and game genre

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -23,5 +23,17 @@ class GenreSeeder extends Seeder
         DB::table('genres')->insert([
             'GameGenre' => 'Racing'
         ]);
+        DB::table('genres')->insert([
+            'GameGenre' => 'RPG'
+        ]);
+        DB::table('genres')->insert([
+            'GameGenre' => 'Survival'
+        ]);
+        DB::table('genres')->insert([
+            'GameGenre' => 'MOBA'
+        ]);
+        DB::table('genres')->insert([
+            'GameGenre' => 'Battle Royale'
+        ]);
     }
 }
