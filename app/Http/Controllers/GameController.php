c7287da7227e21c9d32d000c8198f267fd164175
dwikis17 <dwikis17@gmail.com>
2021-12-07 17:53:02 +0700

    add function insertGame
    add function insertGame

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Game;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class GameController extends Controller
 {
@@ -14,5 +15,25 @@ class GameController extends Controller
         $genres = Genre::all();
         return view('home', compact('games', 'genres'));
       //  return view('home', ['m' =>$movies]);
+    }
+
+    public function insertGame(Request $request){
+        $games = new Game();
+        $games->GameTitle = $request->GameTitle;
+        $games->GameDesc = $request->GameDesc;
+        $games->GamePrice = $request->GamePrice;
+        $games->GenreId = $request->GenreId;
+        $games->GamePegiRating = $request->GamePegiRating;
+        $file = $request->file('GameImage');
+
+        $imageName = time().'.'.$file->getClientOriginalExtension();
+
+        Storage::putFileAs('public/images',$file ,$imageName);
+        
+        $games->GameImage = $imageName;
+        $games->save();
+
+        return redirect()->back();
+
     }
 }
