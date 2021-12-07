3e20a41202c9d69cba5a9b82c7bcc8bffd7983ed
daniellie31 <danielll3112@gmail.com>
2021-12-07 21:00:35 +0700

    manageGameGenre styling

--- resources/views/manageGameGenre.blade.php
+++ resources/views/manageGameGenre.blade.php
@@ -2,33 +2,21 @@
 @section('title', 'Manage Game Genre')
 
 @section('content')
-    <table class="table table-borderless">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-        </tbody>
-    </table>
+    <link rel="stylesheet" href="{{ asset('css/manageGameGenre.css') }}">
+
+    <div class="manageGameGenre-container">
+        <div class="form-container">
+            <table class="table table-borderless">
+                <h2 class="font-weight-bold">Game Genre</h2>
+                <tbody>
+                    @foreach ($genres as $g)
+                        <tr>
+                            <td class="col-12">{{ $g->GameGenre }}</td>
+                            <td><a href="">Edit</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
