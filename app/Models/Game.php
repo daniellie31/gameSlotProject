eb57f2514fbf2cf61dc04e980bbb6c2dac249239
dwikis17 <dwikis17@gmail.com>
2021-12-07 17:53:21 +0700

    change value $timestamps = false

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     public function genres(){
         return $this->belongsTo(Genre::class, 'GenreId', 'GenreId');
