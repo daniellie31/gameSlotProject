093f9b2030e220be2f26c15c2c42ca3648968be5
dwikis17 <dwikis17@gmail.com>
2022-01-03 20:10:18 +0700

    test
    test

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -25,12 +25,7 @@
 @endif
 <form class="form-signin card" action="/signIn" method="post">
     @csrf
-    @if (session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
+  
 
     <form class="form-signin card" action="signIn" method="post">
         @csrf
